dc176dbaa5f8ea2b090ae03c9631b8d5c956dc40
sojiniya <sojin9845@gmail.com>
2022-03-18 17:48:24 +0900

    Movie Review VO 작성

---
+++ springProject/src/main/java/kr/spring/movie/vo/MovieReviewVO.java
@@ -0,0 +1,53 @@
+package kr.spring.movie.vo;
+
+public class MovieReviewVO {
+	private int review_num;
+	private String review_content;
+	private String review_reg_date;
+	private int review_rate;
+	private int movie_num;//부모 글번호
+	private int mem_num;//작성자 회원번호
+	
+	public int getReview_num() {
+		return review_num;
+	}
+	public void setReview_num(int review_num) {
+		this.review_num = review_num;
+	}
+	public String getReview_content() {
+		return review_content;
+	}
+	public void setReview_content(String review_content) {
+		this.review_content = review_content;
+	}
+	public String getReview_reg_date() {
+		return review_reg_date;
+	}
+	public void setReview_reg_date(String review_reg_date) {
+		this.review_reg_date = review_reg_date;
+	}
+	public int getReview_rate() {
+		return review_rate;
+	}
+	public void setReview_rate(int review_rate) {
+		this.review_rate = review_rate;
+	}
+	public int getMovie_num() {
+		return movie_num;
+	}
+	public void setMovie_num(int movie_num) {
+		this.movie_num = movie_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	@Override
+	public String toString() {
+		return "MovieReviewVO [review_num=" + review_num + ", review_content=" + review_content + ", review_reg_date="
+				+ review_reg_date + ", review_rate=" + review_rate + ", movie_num=" + movie_num + ", mem_num=" + mem_num
+				+ "]";
+	}
+}
