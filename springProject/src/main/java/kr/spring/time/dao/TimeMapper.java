bf80b6a0629e55b006cecf541909f51f6d871648
user <user@DESKTOP-VMEID54>
2022-03-22 01:06:32 +0900

    극장리스트 폼 수정중

--- springProject/src/main/java/kr/spring/time/dao/TimeMapper.java
+++ springProject/src/main/java/kr/spring/time/dao/TimeMapper.java
@@ -1,16 +1,13 @@
 package kr.spring.time.dao;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Select;
 
 import kr.spring.time.vo.TimeVO;
 
 public interface TimeMapper {
-	//1.목록 : 총 레코드 수
-	@Select("SELECT COUNT(*) FROM M_time")
-	public int getTimeCount();
-	//1-1.목록 : 리스트
-	public List<TimeVO> selectList(Map<String,Object> map);
+	//1.목록 : 리스트
+	@Select("SELECT * FROM M_time")
+	public List<TimeVO> selectList(TimeVO timeVO);
 }
