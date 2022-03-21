6610bdba71609f174bf01ae737487953d078e24d
user <user@DESKTOP-VMEID54>
2022-03-21 16:39:14 +0900

    수정

--- springProject/src/main/java/kr/spring/theater/controller/TheaterController.java
+++ springProject/src/main/java/kr/spring/theater/controller/TheaterController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import kr.spring.theater.service.TheaterService;
 import kr.spring.theater.vo.TheaterVO;
 
+@Controller
 public class TheaterController {
 	private static final Logger logger = LoggerFactory.getLogger(TheaterController.class);
 	
