0d118295d8a20a64284db20580822582c268ea25
user <user@DESKTOP-VMEID54>
2022-03-28 15:15:26 +0900

    timeVO수정

--- springProject/src/main/java/kr/spring/time/vo/TimeVO.java
+++ springProject/src/main/java/kr/spring/time/vo/TimeVO.java
@@ -6,8 +6,6 @@ import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-
-
 public class TimeVO {
 	private int time_num; //상영시간 번호
 	private int theater_num; //영화관 번호
@@ -19,6 +17,14 @@ public class TimeVO {
 	private Date movie_date; //상영일
 	@NotEmpty
 	private String movie_time; //상영시간
+	
+	//극장정보 등록을 위해 movie테이블에서 가져온 데이터
+	@NotEmpty
+	private String moive_genre; //장르
+	@NotEmpty
+	private int movie_pg; //관람연령(12/15/19)
+	@NotEmpty
+	private int moive_auth; //영화 상영정보 0:상영중/1:상영예정/2:상영종료
 	
 	public int getTime_num() {
 		return time_num;
@@ -56,12 +62,30 @@ public class TimeVO {
 	public void setMovie_time(String movie_time) {
 		this.movie_time = movie_time;
 	}
+	public String getMoive_genre() {
+		return moive_genre;
+	}
+	public void setMoive_genre(String moive_genre) {
+		this.moive_genre = moive_genre;
+	}
+	public int getMovie_pg() {
+		return movie_pg;
+	}
+	public void setMovie_pg(int movie_pg) {
+		this.movie_pg = movie_pg;
+	}
+	public int getMoive_auth() {
+		return moive_auth;
+	}
+	public void setMoive_auth(int moive_auth) {
+		this.moive_auth = moive_auth;
+	}
 	
 	//투스트링
 	@Override
 	public String toString() {
 		return "TimeVO [time_num=" + time_num + ", theater_num=" + theater_num + ", movie_num=" + movie_num
-				+ ", movie_name=" + movie_name + ", movie_date=" + movie_date + ", movie_time=" + movie_time + "]";
+				+ ", movie_name=" + movie_name + ", movie_date=" + movie_date + ", movie_time=" + movie_time
+				+ ", moive_genre=" + moive_genre + ", movie_pg=" + movie_pg + ", moive_auth=" + moive_auth + "]";
 	}
-	
 }
