4554df0dbab2489207b8e1c7d7208549d95beb19
sojiniya <sojin9845@gmail.com>
2022-04-01 15:48:14 +0900

    MovieLikeVO 추가

---
+++ springProject/src/main/java/kr/spring/movie/vo/MovieLikeVO.java
@@ -0,0 +1,32 @@
+package kr.spring.movie.vo;
+
+public class MovieLikeVO {
+	private int my_movie_num;
+	private int movie_num;
+	private int mem_num;
+	
+	public int getMy_movie_num() {
+		return my_movie_num;
+	}
+	public void setMy_movie_num(int my_movie_num) {
+		this.my_movie_num = my_movie_num;
+	}
+	public int getMovie_num() {
+		return movie_num;
+	}
+	public void setMovie_num(int movie_num) {
+		this.movie_num = movie_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	
+	@Override
+	public String toString() {
+		return "MovieLikeVO [my_movie_num=" + my_movie_num + ", movie_num=" + movie_num + ", mem_num=" + mem_num + "]";
+	}
+
+}
