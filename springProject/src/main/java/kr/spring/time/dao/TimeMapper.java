54df6e08d92e9b031770affb6b2e9303fea46112
user <user@DESKTOP-VMEID54>
2022-04-05 22:03:35 +0900

    좌석리스트 sql문 수정 완료

--- springProject/src/main/java/kr/spring/time/dao/TimeMapper.java
+++ springProject/src/main/java/kr/spring/time/dao/TimeMapper.java
@@ -32,7 +32,7 @@ public interface TimeMapper {
 	//3.리스트
 	@Select("select time_num,movie_num,theater_num,t.movie_name,movie_genre,movie_pg,movie_auth,to_char(movie_date,'YYYY/MM/DD') movie_date,movie_time,movie_showtm,theater_name from m_time t JOIN m_theater h using(theater_num) JOIN m_movie USING(movie_num) where movie_date=#{movie_date} and theater_num=#{theater_num}") 
 	public List<TimeVO> selectList(Map<String,Object> map);
-	@Select("select movie_time, total_count, reserv_count, total_count-reserv_count rest_count FROM (select movie_time, (select count(*) from m_seat where theater_num=#{theater_num}) total_count, count(*) reserv_count from m_reservseat r join m_time using(time_num) where theater_num=#{theater_num} and to_char(movie_date,'YYYY/MM/DD')=#{movie_date} group by movie_time)")
+	@Select("select movie_time, total_count, reserv_count, total_count-reserv_count rest_count FROM (select movie_time, (select count(*) from m_seat where theater_num=#{theater_num}) total_count, count(revseat_num) reserv_count from m_reservseat r right outer join m_time using(time_num) where theater_num=#{theater_num} and to_char(movie_date,'YYYY/MM/DD')=#{movie_date} group by movie_time) order by movie_time")
 	public List<TimeVO> selectTimeList(Map<String,Object> map);
 	
 }
